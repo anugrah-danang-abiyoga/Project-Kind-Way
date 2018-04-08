34c437db4d028006849987817fc9ba4696d3fb37
ben-tai <jamin.tairea@gmail.com>
2018-04-08 16:00:14 +1200

    almost done with routes and server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const hbs = require('express-handlebars') //this will be replaced with React
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+const fs = require('fs')
+
+const server = express()
+
+//Middleware 
+
+server.engine('hbs', hbs({
+    defaultLayout: 'main',
+    extname: 'hbs'
+}))
+//hbs will be replaced by react I guess
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+//Routes
+server.use('/', routes)
+
+module.exports = server
