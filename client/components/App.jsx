2dac98786b2de3aa91423d550c183fc2726ab05a
ben-tai <jamin.tairea@gmail.com>
2018-04-12 16:01:35 +1200

    REACT FINALLY ACTIVATED

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,2 +1,10 @@
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom' 
+
+const App = props => {
+    return <Router>
+        <h1>Have you done any good deeds today?</h1>
+    </Router>
+}
+
+export default App
