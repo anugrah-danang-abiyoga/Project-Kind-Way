eb80f869435ab28f77ce9de949af528918331eb7
anugrah danang abiyoga <danangabiyoga26@gmail.com>
2018-04-14 10:32:55 +1200

    testing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 
 const App = props => {
     return <Router>
-        <h1>Have you done any good deeds today?</h1>
+        <h1>Hello, Have you done any good deeds today?</h1>
     </Router>
 }
 
