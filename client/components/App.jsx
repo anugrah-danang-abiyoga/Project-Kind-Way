57a9b902317692ba8202742af3da1048739487d1
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-04-15 13:32:37 +1200

    loading animation in JSX yeay

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom' 
 
+import Loader from './Loader'
+
 const App = props => {
     return <Router>
+        <div>
+            <Loader />
         <h1>Hello, Have you done any good deeds today?</h1>
+        </div>
     </Router>
 }
 
---
+++ client/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Loading from 'react-loading-components';
+ 
+const Loader = () => (
+  <Loading type='ball_triangle' width={100} height={100} fill='#f44242' />
+);
+ 
+export default Loader;
+
